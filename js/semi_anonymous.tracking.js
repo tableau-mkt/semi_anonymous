e53b9e09e18a2acc55c9873b86abd97ae0091458
Josh Lind <jlind@tableausoftware.com>
2014-05-12 19:53:53 -0700

    track and total term hits, reduce to vocabs re #3496

--- js/semi_anonymous.tracking.js
+++ js/semi_anonymous.tracking.js
@@ -14,46 +14,76 @@
     attach: function (context, settings) {
       if (context == document) {
 
-        // Log page visit.
-        if (Drupal.settings.semi_anonymous.track_browsing) {
-          SemiAnon.create_activity('page', window.location.href);
-        }
+        // Log browsing.
+        if (Drupal.settings.semi_anonymous.track_browsing || Drupal.settings.semi_anonymous.track_term_hits) {
+          var return_val = {};
 
-        // Log term hit.
-        if (Drupal.settings.semi_anonymous.track_term_hits) {
-          $each(Drupal.settings.semi_anonymous.taxonomy, function () {
-            SemiAnon.create_activity('term', $(this));
-          });
+          // Log page view.
+          if (Drupal.settings.semi_anonymous.track_browsing) {
+            return_val['url'] = window.location.href;
+          }
+          // Log term hits.
+          if (Drupal.settings.semi_anonymous.track_term_hits) {
+            return_val['taxonomy'] = Drupal.settings.semi_anonymous_meta.taxonomy
+          }
+          // Stash it.
+          SemiAnon.create_activity('browsing', JSON.stringify(return_val));
         }
 
       }
     }
   };
 
-  // Access records of group, which are counted and grouped by distict values.
-  SemiAnon.get_activities_distinct_grouped_count = function(group, distinct) {
-    var results = semi_anon.get_activities(group);
+  // Look through browsing history and find user's top terms.
+  SemiAnon.get_favorite_terms = function() {
+    var results = SemiAnon.get_activities('browsing'),
+        pages = [], // De-dupe.
+        terms = {}; // Return.
 
-    $.each($.jStorage.index(), function(key, r) {
-      // Provide the count.
-      var value = $.jStorage.get(key);
-      if (value == distinct) {
-        results[r].count++;
+    // Walk through tracking records.
+    $.each(results, function(i, key) {
+      var record = JSON.parse($.jStorage.get(key));
+      // Only count once.
+      if (typeof pages[record.url] === 'undefined') {
+        pages[record.url] = true;
+        if (record.hasOwnProperty('taxonomy')) {
+          // Walk through vocabs and terms.
+          $.each(record.taxonomy, function(voc_name, data) {
+            $.each(record.taxonomy[voc_name], function(tid, val) {
+              // Existing, add to count.
+              if (terms.hasOwnProperty(tid)) {
+                terms[tid]['count']++;
+              }
+              else {
+                // New, add it on and create count.
+                terms[tid] = {};
+                terms[tid]['vocabulary'] = voc_name;
+                terms[tid]['count'] = 1;
+              }
+            });
+          });
+        }
+
       }
     });
-    return results;
+    
+    // @todo Sort and reduce.
+
+    return terms;
   };
 
-  // Access records of a specific group.
+  // Access records of a specific tracking group.
   SemiAnon.get_activities = function(group) {
     var results = $.jStorage.index();
+
     if (group != false) {
-      $.each(results, function (i, r) {
-        // Remove unwanted types.
-        if (!r.indexOf(group)) {
-          results.splice(i, 0);
+      // Remove unwanted types (string beginning assumed).
+      for(var i = 0; i < results.length; i++) {
+        if (results[i].indexOf('track_' + group) != 0) {
+          results.splice(i, 1);
+          i--;
         }
-      });
+      };
       return results;
     }
     else {
--- js/semi_anonymous.user.js
+++ js/semi_anonymous.user.js
@@ -9,18 +9,25 @@
     attach: function (context) {
       if (context == document) {
 
-console.log('HAPPENING');
+        // Stash the session entry point.
+        if (!$.jStorage.get('user.origin') || !document.referrer) {
+          var n = new Date().getTime();
+          $.jStorage.set(
+            'user.session_origin',
+            '{ "timestamp" : "' + n + '", ' +
+              '"url" : "' + window.location.href + '" }'
+          );
+        }
 
         // Stash the deep origin.
         if (!$.jStorage.get('user.origin')) {
           var n = new Date().getTime();
-          $.jStorage.set('user.origin', n + '|' + window.location.href);
-        }
-    
-        // Stash the session entry point.
-        if (!document.referrer) {
-          var n = new Date().getTime();
-          $.jStorage.set('user.session_origin', n + '|' + window.location.href);
+          $.jStorage.set(
+            'user.origin',
+            '{ "timestamp" : "' + n + '", ' +
+              '"url" : "' + window.location.href + '", ' +
+              '"referrer" : "' + document.referrer + '"}'
+          );
         }
 
       }
