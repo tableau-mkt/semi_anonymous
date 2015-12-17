d937adf679bd4169415c512b197517951f3dd0e4
Josh Lind <jlind@tableausoftware.com>
2015-12-17 11:34:49 -0800

    better form submit tracking, improved admin UI, code cleanup

--- js/custom-activities.js
+++ js/custom-activities.js
@@ -2,17 +2,32 @@
  * Record custom activities from Drupal norms.
  */
 
-(function ($) {
-  Drupal.behaviors.SemiAnonymousActivitiesFormSubmit = {
+/* global dataLayer, DataLayerHelper */
+
+(function ($, window, dataLayer) {
+  Drupal.behaviors.semiAnonymousFormSubmit = {
     attach: function (context, settings) {
 
-      var submitData = {};
-      $('form').submit(function(e) {
-        submitData.formId = $(this).find('input[name="form_id"]').val();
-        submitData.url = window.location.href;
-        groucho.createActivity('form_submit', submitData);
+      // Ajax protection.
+      if (context !== document) return;
+
+      // Record configured form submits.
+      $('form').submit(function() {
+        var forms = settings.semiAnonymous.recordForms,
+            formId = $(this).find('input[name="form_id"]').val(),
+            dlHelper = new DataLayerHelper(dataLayer);
+
+        if (forms.length === 0 || $.inArray(formId, forms) >= 0) {
+          groucho.createActivity('formSubmit', {
+            formId: formId,
+            url: window.location.href,
+            entityBundle: dlHelper.get('entityBundle'),
+            entityId: dlHelper.get('entityId'),
+            entityTnid: dlHelper.get('entityTnid'),
+          });
+        }
       });
 
     }
   };
-})(jQuery);
+})(jQuery, window, dataLayer);
